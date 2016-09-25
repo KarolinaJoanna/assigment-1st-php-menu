7495cec99e44a10ef656b00c19ca11ece98b1bd4
Karolina Joanna Zdyb <zdybkarolina@gmail.com>
2016-09-25 13:38:20 +0200

    Create page3.php
    alredy menu included

---
+++ page3.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$curpage = 'page3.php';
+
+ require 'menu.php';?>
+<h1>Page 3</h1>
+
+<p>page one text blablabla</p>
+
+<?php require 'footer.php';?>
+</body>
+</html>
