a4ae87cf9aaff294395a4e005d4a70de6eaff4de
Karolina Joanna Zdyb <zdybkarolina@gmail.com>
2016-09-25 13:42:39 +0200

    Create page4.php

---
+++ page4.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$curpage = 'page4.php';
+
+ require 'menu.php';?>
+<h1>Page 4</h1>
+
+<p>page one text blablabla</p>
+
+<?php require 'footer.php';?>
+</body>
+</html>
