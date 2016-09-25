77d58fee4dade36ddf3c33b1932cf00f2d992bbb
Karolina Joanna Zdyb <zdybkarolina@gmail.com>
2016-09-25 13:43:17 +0200

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$curpage = 'page2.php';
+
+ require 'menu.php';?>
+<h1>Page 2</h1>
+
+<p>page one text blablabla</p>
+
+<?php require 'footer.php';?>
+</body>
+</html>
